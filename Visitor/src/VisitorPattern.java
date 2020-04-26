e999b1c71faced5425fd8084e35692989d184cb3
ww118132 <1181328143@qq.com>
2020-04-26 22:50:22 +0800

    访问者

---
+++ Visitor/src/VisitorPattern.java
@@ -0,0 +1,94 @@
+
+import java.util.*;
+public class VisitorPattern
+{
+    public static void main(String[] args)
+    {
+        ObjectStructure os=new ObjectStructure();
+        os.add(new ConcreteElementA());
+        os.add(new ConcreteElementB());
+        Visitor visitor=new ConcreteVisitorA();
+        os.accept(visitor);
+        System.out.println("------------------------");
+        visitor=new ConcreteVisitorB();
+        os.accept(visitor);
+    }
+}
+
+interface Visitor
+{
+    void visit(ConcreteElementA element);
+    void visit(ConcreteElementB element);
+}
+
+class ConcreteVisitorA implements Visitor
+{
+    public void visit(ConcreteElementA element)
+    {
+        System.out.println("具体访问者A访问-->"+element.operationA());
+    }
+    public void visit(ConcreteElementB element)
+    {
+        System.out.println("具体访问者A访问-->"+element.operationB());
+    }
+}
+class ConcreteVisitorB implements Visitor
+{
+    public void visit(ConcreteElementA element)
+    {
+        System.out.println("具体访问者B访问-->"+element.operationA());
+    }
+    public void visit(ConcreteElementB element)
+    {
+        System.out.println("具体访问者B访问-->"+element.operationB());
+    }
+}
+
+interface Element
+{
+    void accept(Visitor visitor);
+}
+class ConcreteElementA implements Element
+{
+    public void accept(Visitor visitor)
+    {
+        visitor.visit(this);
+    }
+    public String operationA()
+    {
+        return "具体元素A的操作。";
+    }
+}
+
+class ConcreteElementB implements Element
+{
+    public void accept(Visitor visitor)
+    {
+        visitor.visit(this);
+    }
+    public String operationB()
+    {
+        return "具体元素B的操作。";
+    }
+}
+
+class ObjectStructure
+{   
+    private List<Element> list=new ArrayList<Element>();   
+    public void accept(Visitor visitor)
+    {
+        Iterator<Element> i=list.iterator();
+        while(i.hasNext())
+        {
+            ((Element) i.next()).accept(visitor);
+        }      
+    }
+    public void add(Element element)
+    {
+        list.add(element);
+    }
+    public void remove(Element element)
+    {
+        list.remove(element);
+    }
+}
