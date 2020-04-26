df357eccc50d697e6cfd8dd173fa9f169b57a6c1
ww118132 <1181328143@qq.com>
2020-04-26 22:46:33 +0800

    模板

---
+++ TemplateMethod/src/TemplateMethodPattern.java
@@ -0,0 +1,37 @@
+
+public class TemplateMethodPattern
+{
+    public static void main(String[] args)
+    {
+        AbstractClass tm=new ConcreteClass();
+        tm.TemplateMethod();
+    }
+}
+
+abstract class AbstractClass
+{
+    public void TemplateMethod() 
+    {
+        SpecificMethod();
+        abstractMethod1();          
+         abstractMethod2();
+    }  
+    public void SpecificMethod() 
+    {
+        System.out.println("抽象类中的具体方法被调用...");
+    }   
+    public abstract void abstractMethod1(); 
+    public abstract void abstractMethod2(); 
+}
+
+class ConcreteClass extends AbstractClass
+{
+    public void abstractMethod1()
+    {
+        System.out.println("抽象方法1的实现被调用...");
+    }   
+    public void abstractMethod2()
+    {
+        System.out.println("抽象方法2的实现被调用...");
+    }
+}
